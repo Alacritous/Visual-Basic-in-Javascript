bf2cbee1a38fea1dd5bdc7f083ca2ed29b714321
Alacritous <aj.barker@gmail.com>
2012-03-07 20:23:33 -0700

    Update vb.js

--- vb.js
+++ vb.js
@@ -5,58 +5,57 @@
 // All the functions can be accessed through the namespace vb
 // like vb.chr(65);
 // will return an "A"
-// this file will occasionally be passed through  http://www.jslint.com/
-// to make sure it's okee dokee. 
+//
 
 var vb = vb || {}; //define vb namespace here
 //
 //string functions
 //
-vb.trim = function(stringToTrim) {
-    return stringToTrim.replace(/^\s+|\s+$/g,"");
-}
-vb.ltrim = function(stringToTrim) {
-    return stringToTrim.replace(/^\s+/,"");
-}
-vb.rtrim = function(stringToTrim) {
-    return stringToTrim.replace(/\s+$/,"");
-}
-vb.strreverse = function(string){
+vb.trim = function (stringToTrim) {
+    return stringToTrim.replace(/^\s+|\s+$/g, "");
+};
+vb.ltrim = function (stringToTrim) {
+    return stringToTrim.replace(/^\s+/, "");
+};
+vb.rtrim = function (stringToTrim) {
+    return stringToTrim.replace(/\s+$/, "");
+};
+vb.strreverse = function (string) {
     return string.split("").reverse().join("");
-}
-vb.mid$ = function(string, start, count) {
+};
+vb.mid$ = function (string, start, count) {
     return string.substring(start, start + count);
-}
-vb.instr = function(string, substring) {
-    return string.indexOf(substring)
-}
-vb.instrrev = function(string, substring) {
+};
+vb.instr = function (string, substring) {
+    return string.indexOf(substring);
+};
+vb.instrrev = function (string, substring) {
     return string.lastIndexOf(substring);
-}
-vb.chr = function(asciinum) {
+};
+vb.chr = function (asciinum) {
     return String.fromCharCode(asciinum);
-}
-vb.asc = function(string) {
+};
+vb.asc = function (string) {
     return string.charCodeAt(0);
-}
-vb.lcase = function(string){
+};
+vb.lcase = function (string) {
     return string.toLowerCase();
-}
-vb.ucase = function(string){
+};
+vb.ucase = function (string) {
     return string.toUpperCase();
-}
-vb.replace = function(string,find,newtext){
-    newstring = "/" + find + "/gi";
-    newstring.replace(/;/gi,"");
-  return string.replace( eval(newstring),newtext);
-}
+};
+vb.replace = function (string, find, newtext) {
+    var newstring = "/" + find + "/gi";
+    newstring.replace(/;/gi, "");
+    return string.replace(eval(newstring), newtext);
+};
 //
 //number functions
 //
-vb.abs = function(number){
+vb.abs = function (number) {
     return Math.abs(number);
-}
-vb.hex = function(d, padding) { //returns hexadecimal value from integer input. with optional 0 padding
+};
+vb.hex = function (d, padding) { //returns hexadecimal value from integer input. with optional 0 padding
     var hex = Number(d).toString(16);
     padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
 
@@ -64,32 +63,36 @@ vb.hex = function(d, padding) { //returns hexadecimal value from integer input. 
         hex = "0" + hex;
     }
     return hex.toUpperCase();
-}
-vb.dec = function(hex){  //returns decimal value from hexadecimal input
-   return parseInt(hex,16);
-}
-vb.val = function(string) {//returns floating point value with decimals.
+};
+vb.dec = function (hex) {  //returns decimal value from hexadecimal input
+    return parseInt(hex, 16);
+};
+vb.val = function (string) {//returns floating point value with decimals.
     return parseFloat(string);
+};
+vb.intval = function (string) { //returns integer value with no decimals. 
+    return parseInt(string, 10);
+};
+vb.int = function(value){
+return parseInt(value);    
 }
-vb.intval = function(string) { //returns integer value with no decimals. 
-    return parseInt(string,10);
-}
+
 //
 //array functions
 //
-vb.split = function(content,delimiter){ //returns array from content divided by delimeter. 
+vb.split = function (content, delimiter) { //returns array from content divided by delimeter. 
     return content.split(delimiter);
-}
+};
 //
 //date functions
 //
-vb.date = function(){
+vb.date = function () {
     var d = new Date(Date.now());
-     return vb.val(d.getMonth())+1 + "/" + d.getDate() + "/" + d.getFullYear()
-}
+    return vb.val(d.getMonth()) + 1 + "/" + d.getDate() + "/" + d.getFullYear();
+};
 //misc functions
-vb.qq = function(string) {//returns string wrapped in double quotes " at both ends. 
+vb.qq = function (string) {//returns string wrapped in double quotes " at both ends. 
     return String.fromCharCode(34) + string + String.fromCharCode(34);
-}
+};
 // Constants
 var vbcrlf = vb.chr(10) + vb.chr(13);
